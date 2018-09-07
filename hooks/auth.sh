5ccb7f0c3ce10c06e769b5a386d334c0f72f23aa
Bacon, Josh <Josh.L.Bacon@nordstrom.com>
2018-09-07 15:30:54 -0700

    Add acmeSeret access to the role. Fix kubectl permissions.

--- hooks/auth.sh
+++ hooks/auth.sh
@@ -3,6 +3,7 @@ set -e
 echo ${CERTBOT_VALIDATION} > ./${CERTBOT_TOKEN}
 apk add --no-cache curl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x kubectl
 ./kubectl create secret generic ${ACME_SECRET_NAME} \
 --from-file=${CERTBOT_TOKEN}=${PWD}/${CERTBOT_TOKEN} \
 --dry-run \
--- hooks/cleanup.sh
+++ hooks/cleanup.sh
@@ -3,6 +3,7 @@ set -e
 rm -f ./${CERTBOT_TOKEN}
 apk add --no-cache curl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x kubectlkubec    get 
 ./kubectl create secret generic ${CERT_SECRET_NAME} \
 --from-file=cert.pem=/root/cert.pem \
 --from-file=privkey.pem=/root/privkey.pem \
