0e249ee851f973ff0ebe016ab56d2e6a4534a92e
Bacon, Josh <Josh.L.Bacon@nordstrom.com>
2018-09-07 12:38:22 -0700

    Removes email list because trailing comma. Adds kubectl install scripting. Changes restartPolicy so I can see the logs.

--- hooks/auth.sh
+++ hooks/auth.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -e
 echo ${CERTBOT_VALIDATION} > ./${CERTBOT_TOKEN}
-kubectl create secret generic ${ACME_SECRET_NAME} \
+apk add --no-cache curl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+./kubectl create secret generic ${ACME_SECRET_NAME} \
 --from-file=${CERTBOT_TOKEN}=${PWD}/${CERTBOT_TOKEN} \
 --dry-run \
 -o yaml \
-| kubectl apply -f -
+| ./kubectl apply -f -
 sleep 30
--- hooks/cleanup.sh
+++ hooks/cleanup.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 set -e
 rm -f ./${CERTBOT_TOKEN}
-kubectl create secret generic ${CERT_SECRET_NAME} \
+apk add --no-cache curl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+./kubectl create secret generic ${CERT_SECRET_NAME} \
 --from-file=cert.pem=/root/cert.pem \
 --from-file=privkey.pem=/root/privkey.pem \
 --from-file=fullchain.pem=/root/fullchain.pem \
 --from-file=chain.pem=/root/chain.pem \
 --dry-run \
 -o yaml \
-| kubectl apply -f -
-kubectl create secret tls ${TLS_SECRET_NAME} \
+| ./kubectl apply -f -
+./kubectl create secret tls ${TLS_SECRET_NAME} \
 --cert=/root/fullchain.pem \
 --key=/root/privkey.pem \
 --dry-run \
 -o yaml \
-| kubectl apply -f -
+| ./kubectl apply -f -
 sleep 30
