6fb764f963a01b803487cc057abd5389278dc318
Bacon, Josh <Josh.L.Bacon@nordstrom.com>
2018-09-18 13:01:18 -0700

    Fixed s3 hook bucket and command arguments

--- hooks/aws-s3/auth.sh
+++ hooks/aws-s3/auth.sh
@@ -2,5 +2,5 @@
 set -e
 pip install awscli
 echo -n ${CERTBOT_VALIDATION} >> ${PWD}/${CERTBOT_TOKEN}
-aws s3 cp "${PWD}/${CERTBOT_TOKEN}" "s3://${AWS_BUCKET_NAME}/.well-known/acme-challenge/${CERTBOT_TOKEN}"
+aws s3 cp "${PWD}/${CERTBOT_TOKEN}" "s3://${AWS_S3_BUCKET_NAME}/.well-known/acme-challenge/${CERTBOT_TOKEN}"
 sleep 30
--- hooks/aws-s3/cleanup.sh
+++ hooks/aws-s3/cleanup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -e
-aws s3 rm "s3://${AWS_BUCKET_NAME}/.well-known/acme-challenge/${CERTBOT_TOKEN}"
+aws s3 rm "s3://${AWS_S3_BUCKET_NAME}/.well-known/acme-challenge/${CERTBOT_TOKEN}"
